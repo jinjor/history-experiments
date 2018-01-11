3b01fe5810bb8531f2b6bcc2c4c4a144042ad0e3
jinjor <jinjorweb@gmail.com>
2018-01-11 13:53:08 +0900

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Emulate fetching data from server which takes a long time.
 function getData(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -9,6 +10,12 @@ function getData(id) {
   });
 }
 
+// Each route definition has 3 functions.
+//
+// 1. `match: urlstring => boolean`
+// 2. `init: urlstring => Promise<data>`
+// 3. `render: data => string`
+//
 const routes = [{
   match: path => path.endsWith('/a'),
   init: path => getData(1),
@@ -34,9 +41,11 @@ function getRoute(path) {
 }
 
 let time = 0;
-
+// This function wraps route.init and judge if url has been changed during loading.
+// If the url has been changed, the loaded data will be thrown away.
 function init(route, path, state) {
   time++;
+  // If history has state, immediately resolve with it.
   if (state) {
     return Promise.resolve(state);
   }
@@ -52,21 +61,26 @@ function init(route, path, state) {
 
 function doRouting(path, state) {
   const route = getRoute(path);
-  if (route) {
-    out.textContent = 'loading...';
-    init(route, path, state).then(data => {
-      history.replaceState(data, null, path);
-      out.textContent = route.render(data);
-    }).catch(() => {});
+  if (!route) {
+    return;
   }
+  out.textContent = 'loading...';
+  init(route, path, state).then(data => {
+    // Override state with loaded data without pushing new history.
+    history.replaceState(data, null, path);
+    out.textContent = route.render(data);
+  }).catch(() => {});
 }
 
 function handleClick(e) {
+  // Disable browser's default navigation.
   e.preventDefault();
   const path = e.target.href;
   if (path === location.href) {
     return;
   }
+  // If url changed, push new history and wait for data that will be
+  // asynchronously loaded.
   history.pushState(null, null, path);
   doRouting(path);
 }
@@ -75,8 +89,10 @@ function handleClick(e) {
   element.addEventListener('click', handleClick);
 });
 
+// Subscribe back/forward action.
 window.addEventListener('popstate', e => {
   doRouting(location.pathname, e.state);
 });
 
+// Initial navigation.
 doRouting(location.pathname)
